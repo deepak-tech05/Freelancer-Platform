cd8abe24516f08b43a231766966dafab66fc182e
deepak-tech05 <iamdeepak2222@gmail.com>
2024-01-15 10:11:34 +0530

    Correct Profile Component has been Commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Homepage } from "./Components/Homepage.jsx";
 import { Walletinformation } from "./Components/walletinformation.jsx";
-import { Createprofile } from "./Components/createprofile.jsx";
 import { Workexperience } from "./Components/workexperience.jsx";
+import { Profile } from "./Components/Profile/createprofile.jsx";
 
 function App() {
   return (
     <div>
       {/* <Workexperience /> */}
       {/* <Homepage /> */}
-      {/* <Createprofile /> */}
       {/* <Walletinformation /> */}
+      <Profile />
     </div>
   );
 }
